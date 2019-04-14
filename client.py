8b11d225705160c20a8a0c967a68d841b7663498
OrestisNer <orestis.ner@gmail.com>
2019-04-15 00:49:18 +0300

    Thread handles client socket. First implementation of write

--- client.py
+++ client.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3           # This is client.py file
 import socket
+import json
 
 class Client:
 
-    def __init__(self,host,port):
-        self.host = host;
-        self.port = port;
+    def __init__(self,host,port,type):
+        self.host = host
+        self.port = port
         self.BUFFER_SIZE = 1024
+        self.client_type = type
 
     def establish_connection(self):
         conn_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         conn_socket.connect((self.host, self.port))
         while(True):
-            response = conn_socket.recv(self.BUFFER_SIZE).decode('utf-8')
-            print(response)
-            message = input("Message : ")
-            if(message == "EXIT" or message == ''):
+            prompt = conn_socket.recv(self.BUFFER_SIZE).decode('utf-8')
+            print(prompt)
+
+            verb = input("Action: ")
+            if(verb == "EXIT" or verb == ''):
                 conn_socket.close()
                 break
-            conn_socket.sendall(message.encode('utf-8'))
+            code = input("Code: ")
+            if(verb == "write" or verb == "update"):
+                state = input("State: ")
+                time = input("Time: ")
+                package = {"verb": verb, "code":code,
+                           "state":state, "time":time ,
+                           "client_type":self.client_type}
+            else:
+                package = {"verb": verb , "code":code ,
+                       "client_type":self.client_type}
+
+            conn_socket.sendall(json.dumps(package).encode('utf-8'))
+
             response = conn_socket.recv(self.BUFFER_SIZE).decode('utf-8')
             print(f"{str(response)}")
 
 def main():
-    client = Client(socket.gethostname(),9999)
+    #client = Client(socket.gethostname(),9999,"reader")
+    client = Client(socket.gethostname(),9999,"writer")
     client.establish_connection()
 
 
--- server.py
+++ server.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3           # This is server.py file
 import socket
+import threading
+import json
 
 class Server:
 
-    def __init__(self,host,port):
-        self.host = host
-        self.port = port
+    def __init__(self):
+        self.host = socket.gethostname()
+        self.port = 9999
         self.BUFFER_SIZE = 1024
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serversocket.bind((self.host, self.port))
+        self.serversocket.listen(5)
+        self.airport_data = dict()
+
 
     def start(self):
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # bind to the port
-        serversocket.bind((self.host, self.port))
-        # queue up to 5 requests
-        serversocket.listen(5)
-
         while True:
-           # establish a connection
-           clientsocket,addr = serversocket.accept()
-           #clientsocket.send(self.show_prompt().encode('utf-8'))
+           client_socket,addr = self.serversocket.accept()
            print("Got a connection from %s" % str(addr))
-           client_message = clientsocket.recv(self.BUFFER_SIZE).decode('utf-8')
-           print(client_message)
-           server_response = "From server: "+client_message
-           clientsocket.send(server_response.encode('utf-8'))
-           clientsocket.close()
+           threading.Thread(target=self.open_connetion, args=(client_socket, )).start()
 
     def show_prompt(self):
         return """
@@ -33,10 +29,46 @@ class Server:
                 3 - Τροποποίηση
                 """
 
-def main():
-    server = Server(socket.gethostname(),9999)
-    server.start()
+    def open_connetion(self,client_socket):
+        while(True):
+            client_socket.send(self.show_prompt().encode('utf-8'))
+            client_package_json = client_socket.recv(self.BUFFER_SIZE).decode('utf-8')
+            if(client_package_json == "EXIT"):
+                break
+
+            server_response = self.handle_package(client_package_json)
+            client_socket.send(server_response.encode('utf-8'))
+
+        client_socket.close()
+
+    def handle_package(self,client_package_json):
+        client_data = json.loads(client_package_json)
+        verb = client_data["verb"]
+        client_type = client_data["client_type"]
+        code = client_data["code"]
+        if(verb == "read"):
+            pass
+        elif(client_type == "writer"):
+            if(verb == "delete"):
+                pass
+            elif(verb == "write"):
+                state = client_data["state"]
+                time = client_data["time"]
+                return self.write_data(code,state,time)
+            elif(verb == "update"):
+                pass
+
+        return "Unavailabe action."
+        #return client_type
+
+    def write_data(self,code,state,time):
+        #TODO place a semaphore lock into list
+
+        self.airport_data["code"] = {"code":code, "state": state,
+                                     "time": time, "lock": threading.Lock()}
+
+
 
 
 if __name__ == "__main__":
-    main()
+    Server().start()
